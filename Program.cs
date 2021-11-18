21b8e704e4a7a18b83668114067fbeca19526889
Hannah Stern <92752125+hannahkers@users.noreply.github.com>
2021-11-18 11:44:01 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TipCalculator
+{
+    class Program
+    {
+        static void Main()
+        {
+            double billTotal = 0;
+            double tipPercentage = 0;
+            double tip = 0;
+            double finalBillAmount = 0;
+
+            Console.WriteLine("How much is your bill?");
+
+            billTotal = Utility.GetNumberFromUser();
+
+            Console.WriteLine("How much would you like to tip? Please enter a percent between 1 and 100.");
+
+            tipPercentage = Utility.GetPercentFromUser();
+
+            tip = tipPercentage / 100 * billTotal;
+
+            finalBillAmount = billTotal + tip;
+
+            Console.WriteLine($"Your tip amount is: {tip:C}.");
+
+            Console.WriteLine($"Your final bill amount is: {finalBillAmount:C}");
+
+        }
+    }
+}
---
+++ Utility.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipCalculator
+{
+    static class Utility
+    {
+        public static double GetNumberFromUser()
+        {
+           
+            try
+            {
+                double total = Convert.ToDouble(Console.ReadLine());
+                if (total > 0 )
+                {
+                    return total;
+                }
+                else
+                {
+                    return GetNumberFromUser();
+                }
+               
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter a valid number");
+
+            }
+            return GetNumberFromUser();
+        }
+        public static double GetPercentFromUser()
+        {
+
+            try
+            {
+                double total = Convert.ToDouble(Console.ReadLine());
+                if (total > 0 && total <= 100)
+                {
+                    return total;
+                }
+                else
+                {
+                    return GetPercentFromUser();
+                }
+
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter a valid percent");
+
+            }
+            return GetPercentFromUser();
+        }
+    }
+}
